a9c0621586744dcf67a4f76f1d0570d920f77c1f
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2019-05-29 16:12:42 +0530

    initial commit

---
+++ __tests__/RequestButton-test.tsx
@@ -0,0 +1,28 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import {RequestButton} from "../src/RequestButton";
+import {Action, RequestData} from "@bloomprotocol/share-kit";
+
+const requestData: RequestData = {
+  action: Action.attestation,
+  token: 'a08714b92346a1bba4262ed575d23de3ff3e6b5480ad0e1c82c011bab0411fdf',
+  url: 'https://receive-kit.bloom.co/api/receive',
+  org_logo_url: 'https://bloom.co/images/notif/bloom-logo.png',
+  org_name: 'Bloom',
+  org_usage_policy_url: 'https://bloom.co/legal/terms',
+  org_privacy_policy_url: 'https://bloom.co/legal/privacy',
+  types: ['phone', 'email'],
+};
+const buttonCallbackUrl = 'https://mysite.com/bloom-callback';
+
+
+it('renders correctly', () => {
+  renderer.create(<RequestButton requestData={requestData} buttonCallbackUrl={buttonCallbackUrl} />);
+});
